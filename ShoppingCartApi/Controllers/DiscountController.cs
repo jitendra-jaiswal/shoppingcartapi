b5cb14d53f87a9c7bc5cc6ff884b4fa525ef4b7b
jitendra <jitendra.jaiswal@hotmail.com>
2024-05-10 09:08:09 +0530

    Adding working sql scripts

---
+++ ShoppingCartApi/Controllers/DiscountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingCartApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class DiscountController : ControllerBase
+    {
+        public DiscountController()
+        {
+            
+        }
+        [HttpGet]
+        public string Get(int userid)
+        {
+            return "Hello";
+        }
+    }
+}
---
+++ ShoppingCartApi/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Domain.Responses;
+
+namespace ShoppingCartApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+                
+        }
+
+        [HttpGet]
+        public string Get(int userid)
+        {
+            return "Hello";
+        }
+    }
+}
